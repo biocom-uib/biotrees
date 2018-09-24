a7b108424d3edf8831f5b604571b97f3e24e9ab0
Gabriel Riera <gabriel.riera@uib.es>
2018-09-24 18:46:41 +0200

    yule fixes (missing bits from the refactor) and new actual simulation functions

--- src/biotrees/shape/yule.py
+++ src/biotrees/shape/yule.py
@@ -1,7 +1,20 @@
-from biotrees.phylotree import shape_to_phylotree
-from biotrees.phylotree.yule import yule as phylo_yule
+from functools import lru_cache
+
+from biotrees.phylotree import shape_to_phylotree, phylotree_to_shape
+import biotrees.phylotree.yule as phylo_yule
+from biotrees.util import and_then, parametric_total_probabilities
 
 
+
+def sim_yule_from_t(t):
+    return phylotree_to_shape(phylo_yule.sim_yule_from_t(shape_to_phylotree(t)))
+
+
+def sim_yule(n):
+    return phylotree_to_shape(phylo_yule.sim_yule(n))
+
+
+@and_then(parametric_total_probabilities)
 def yule_from_t(sh, prob):
     """
     Returns a list of tuples containing each shape obtained from `Shape` sh (assuming sh has probability prob) with
@@ -10,11 +23,12 @@ def yule_from_t(sh, prob):
     :param prob: `function` instance.
     :return: `list` instance.
     """
-    t = shape_to_phylotree(sh)
-    tps = [(ti.shape(), p) for ti, p in phylo_yule.yule_from_t(t, prob)]
-    return phylo_yule.collapse_tree_prob_list(tps)
+    for t, p in phylo_yule.yule_from_t(shape_to_phylotree(sh), prob):
+        yield phylotree_to_shape(t), p
 
 
+@lru_cache(maxsize=-1)
+@and_then(parametric_total_probabilities)
 def yule(n):
     """
     Returns a list of tuples containing all the shapes of n leaves that can be obtained under the Yule model and their
@@ -22,5 +36,5 @@ def yule(n):
     :param n: `int` instance.
     :return: `list` instance.
     """
-    tps = [(ti.shape(), p) for ti, p in PhyloTree.Yule.pseudo_yule(n)]
-    return phylo_yule.collapse_tree_prob_list(tps)
+    for t, p in phylo_yule.yule(n):
+        yield phylotree_to_shape(t), p
