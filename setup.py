7737055d4e34f1743c26e4e1ba1bf4132fdf8d8a
Tomás M. Coronado <tomasmartinezcoronado@gmail.com>
2019-03-28 12:16:01 +0100

    Packaging (#7)
    Setup for packaging

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="biotrees",
+    version="1.0.0",
+    author="Tomás Martínez Coronado, Gabriel Riera",
+    author_email="t.martinez@uib.eu, gabriel.riera@uib.es",
+    description="A set of tools to work with phylogenetic trees",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/biocom-uib/biotrees",
+    packages=['biotrees', 'biotrees.shape', 'biotrees.shape.balance', 'biotrees.phylotree'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU Lesser General Public License v3 or later (LGPLv3+)",
+        "Operating System :: OS Independent",
+    ],
+)
+
