a804bb576ee0bd5e65b0b4420039b7a2e73f29f9
tomasmcoronado <t.martinez@uib.eu>
2019-03-28 12:13:19 +0100

    add submodules to setup.py

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/biocom-uib/biotrees",
-    packages=["biotrees"],
+    packages=['biotrees', 'biotrees.shape', 'biotrees.shape.balance', 'biotrees.phylotree'],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: GNU Lesser General Public License v3 or later (LGPLv3+)",
